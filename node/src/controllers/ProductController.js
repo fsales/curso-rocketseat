14287cc78898dfa5d6f6f314642ef9097d840056
Fábio Sales <fabio.oliveira.sales@gmail.com>
2020-05-07 23:14:18 -0300

    configurando o volume dos docker

--- node/src/controllers/ProductController.js
+++ node/src/controllers/ProductController.js
@@ -3,6 +3,12 @@ const Product = mongoose.model('Product');
 
 module.exports = {
   async index(req, res) {
+   /* const produ = await Product.create({
+      title: 'React',
+      description: 'Build',
+      url: 'teste.com.br'
+      });*/
+
     const products = await Product.find();
 
     return res.json(products);
