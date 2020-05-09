4b1765acdfb60b827c9567b7684648e931c74cbe
Fábio Sales <fabio.oliveira.sales@gmail.com>
2020-05-09 09:54:06 -0300

    criando um crud

--- node/server.js
+++ node/server.js
@@ -5,6 +5,9 @@ const requireDir = require('require-dir');
 // iniciando o app
 const app = express();
 
+// permitir o envio de dados no formato de json
+app.use(express.json());
+
 // iniciando mongodb
 mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true, useUnifiedTopology: true });
 
