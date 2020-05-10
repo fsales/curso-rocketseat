2249452e968c965065ceefec1276cfafd69394d5
Fábio Sales <fabio.oliveira.sales@gmail.com>
2020-05-10 13:59:03 -0300

    listagem de produtos

--- react/huntweb/src/pages/main/index.js
+++ react/huntweb/src/pages/main/index.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
-
+import './styles.css';
 export default class Main extends Component {
+
+  state = {
+    products: [],
+  }
 
   componentDidMount() {
     /*executa ação após a criação do componente*/
@@ -10,10 +14,24 @@ export default class Main extends Component {
 
   loadProducts = async () => {
     const response = await api.get('/products');
-    console.log(response);
+    this.setState({ products: response.data.docs });
   };
 
   render() {
-    return <h1>hello</h1>
+    const { products } = this.state;
+    return (
+      <div className='product-list'>
+        {products.map(product => 
+          (
+            <article key={product._id}>
+              <strong>{product.title}</strong>
+              <p>{product.title}</p>
+
+              <a href="#">Acessar</a>
+            </article>
+          )
+        )}
+      </div>
+    )
   }
 }
