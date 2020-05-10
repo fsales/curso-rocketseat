abf569ce6075ad10e9ec6b7d40be014834551c9f
Fábio Sales <fabio.oliveira.sales@gmail.com>
2020-05-10 14:44:35 -0300

    configurando roteamento

--- react/huntweb/src/routes.js
+++ react/huntweb/src/routes.js
@@ -1,15 +1,15 @@
-import react from 'react';
+import React from 'react';
 
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 import Main from './pages/main';
 
-const Routes = () =>{
+const Routes = () =>(
   <BrowserRouter>
     <Switch>
       <Route path="/" component={Main} />
     </Switch>
   </BrowserRouter>
-};
+);
 
 export default Routes;
